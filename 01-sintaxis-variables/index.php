930e8543feebc33bd092bd86b9c8470664c6b902
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2024-05-10 15:58:18 -0500

    feat: ejercicios condicionales

--- 01-sintaxis-variables/index.php
+++ 01-sintaxis-variables/index.php
@@ -1,5 +1,25 @@
 <?php
+echo 'Ejecutando sintaxis variables..';
+?>
 
-echo 'Ejecutando sintaxis variables...';
 
+<?php
+
+$nombre_1 = "Juan";
+$nombre_2 = "Samuel";
+
+echo " Hola $nombre_1 y $nombre_2, ¿Como estan? <br> <br> Pagina creada por: <br> <br> - $nombre_1 <br> <br> -$nombre_2 <br> <br>" ;
+?>
+
+<?php
+
+$mi_numero = 8;
+$respuesta = $mi_numero;
+$respuesta += 2;
+$respuesta *= 2;
+$respuesta -=2;
+$respuesta /= 2;
+$respuesta -= $mi_numero;
+
+echo $respuesta;
 ?>
--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -1,5 +1,26 @@
-<?php
+<!-- Ejercicio 1  1.1-->
 
-echo 'Ejecutando sintaxis condicionales...';
+<?php
+    echo 'Ejecutando condicionales..';
+    echo"<br/>";
+    $articulos = "8";
+
+    if($articulos < 12){
+        echo "Caja rapida";
+    } else {
+        echo "Caja normal";
+     }
+
+
+     /*  Ejercicio 1  1.2 */
+
+     $años ="17";
+
+     if ($años < 18){
+        echo"Puede votar";
+     }
+
+
 
 ?>
+
