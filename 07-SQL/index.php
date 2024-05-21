964c081c47fc2b204e8944739fd46466f01c0634
Juan jose <alvarezzapatajuanjose03@gmail.com>
2024-05-21 16:03:10 -0500

    feat: ejercicios SQL

---
+++ 07-SQL/index.php
@@ -0,0 +1,24 @@
+<?php
+
+//SQL Syntax
+
+// 1. SQL Select
+// Ejemplo: SELECT `ID`, `Edad`, `Documento` FROM `estudiante` WHERE 1;
+//Selcecciona todos los registros de ID, Edad y Documentos dentro de la tabla de estudiante
+
+// 2. SQL SELECT DISTINCT
+//
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
